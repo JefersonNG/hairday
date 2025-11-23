530e9973dcc292bd48f2307b487978936daab0c1
jeferson nascimento <jeffersom732@gmail.com>
2025-11-22 22:26:17 -0400

    config webpack com babel

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin")
+const copyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "src/js/app[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+      publicPath: "src/assets"
+    },
+    port: 9000,
+    open: true
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+    template: path.resolve(__dirname, "index.html"),
+    filename: "index.html"
+  }),
+    new copyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"), 
+          to: "src/assets"
+        }
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
